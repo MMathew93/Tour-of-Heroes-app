7323f111dc2f0eb0aa9554a8779459c71321eb27
lyndexer <mckinney.m93@gmail.com>
2020-06-06 17:15:59 -0700

    Another practice project using ANgular

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 13, name: 'Dr Girlfriend' },
+    { id: 14, name: 'HenchMan 21' },
+    { id: 15, name: 'The Monarch' },
+    { id: 16, name: 'Monstroso' },
+    { id: 18, name: 'QuizBoy' },
+    { id: 21, name: 'Sgt. Hatred' },
+    { id: 23, name: 'Brock Samson' },
+    { id: 24, name: 'Dr Orpheus' },
+    { id: 30, name: 'Phantom Limb' }
+];
